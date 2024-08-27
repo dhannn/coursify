30371b492374c349fb8a7610d06f7b4e88eae4e9
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-08-27 12:46:21 +0800

    Add configuration for different environments

--- pdf_saver.py
+++ pdf_saver.py
@@ -2,8 +2,15 @@ import os
 import pdfkit
 
 def save_as_pdf(title: str, html):
-    # config = pdfkit.configuration(wkhtmltopdf=f'{ os.getenv("NIXPACKS_PKG") }/wkhtmltopdf.exe')
+
+    environment = os.environ.get('ENVIRONMENT', 'dev')
+
     filename = '_'.join(title.split(' ')) + '.pdf'
-    pdfkit.from_string(html, filename, css='static/style.css')
+    if environment == 'dev':
+        config = pdfkit.configuration(wkhtmltopdf=f'bin/wkhtmltopdf.exe')
+        pdfkit.from_string(html, filename, configuration=config, css='/static/style.css')
+    else:
+        pdfkit.from_string(html, filename, css='/static/style.css')
+        
 
     return filename
