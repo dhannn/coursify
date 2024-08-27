1775980b93b8585591582a17efb66ecc99013a61
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2024-08-27 12:24:36 +0800

    Fix deployment issue around binary

--- pdf_saver.py
+++ pdf_saver.py
@@ -1,7 +1,8 @@
+import os
 import pdfkit
 
 def save_as_pdf(title: str, html):
-    config = pdfkit.configuration(wkhtmltopdf='bin/wkhtmltopdf.exe')
+    config = pdfkit.configuration(wkhtmltopdf=f'{ os.getenv("NIXPACKS_PKG") }/wkhtmltopdf.exe')
     filename = '_'.join(title.split(' ')) + '.pdf'
     pdfkit.from_string(html, filename, configuration=config, css='static/style.css')
 
